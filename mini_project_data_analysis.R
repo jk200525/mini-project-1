cbf5637e1e4f31b1cc1fb1756486a8c2095a21e9
Jasmeet Kaur <jkaurjal998@gmail.com>
2025-05-09 23:22:40 +0530

    Add files via upload

--- mini_project_data_analysis.R
+++ mini_project_data_analysis.R
@@ -1,4 +1,6 @@
 # Data Analysis
+library(dplyr)
+library(tidyverse)
 # Importing the file
 anime = read.csv("D:/BCA sem 4/Data Science/mini-project1 (Anime analysis)/anime.csv")
 
@@ -22,5 +24,20 @@ sd_satisfaction <- sd(anime_clean$How.satisfied.are.you.with..current.availabili
 # Calculate quantiles of satisfaction level
 satisfaction_quantiles <- quantile(anime_clean$How.satisfied.are.you.with..current.availability.of.anime.in.your.region., na.rm = TRUE)
 
-# Display summary statistics of the dataset
+# Average satisfaction by gender
+anime_clean %>%
+  group_by(Gender.) %>%
+  summarise(Average_Satisfaction = mean(How.satisfied.are.you.with..current.availability.of.anime.in.your.region., na.rm = TRUE))
+
+# Average satisfaction by age
+anime_clean %>%
+  group_by(Age.) %>%
+  summarise(Average_Satisfaction = mean(How.satisfied.are.you.with..current.availability.of.anime.in.your.region., na.rm = TRUE))
+
+# Create a contingency table: Chi-square test for association between gender and watching status
+table_gender_watch <- table(anime_clean$Gender., anime_clean$Are.you.an.anime.watcher.)
+chisq.test(table_gender_watch)
+table_gender_watch
+
+# Display summary statistics of the data set
 summary(anime_clean)
